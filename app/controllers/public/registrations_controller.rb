e5339a4ce83f2ea92db0784170b27cc4730572bd
YuukiTanaka <145189789+yuukitanaka006607@users.noreply.github.com>
2023-12-12 01:12:26 +0000

    [registration_public]

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -41,9 +41,33 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+  end
+  
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+  end
+  
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
+  end
+  
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
+  end
+  
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code])
+  end
+  
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+  end
+  
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:telephone_naumber])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
