9e453a6861ded9321bd5a534d8efb208d6ee2c6c
YuukiTanaka <145189789+yuukitanaka006607@users.noreply.github.com>
2023-12-21 00:14:34 +0000

    [items_new]

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
    end
 
    namespace :admin do
-    resources :items, only: [:new, :index, :show]
+    resources :items, only: [:new, :create, :index, :show ]
     get "/" => "homes#top"
    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
