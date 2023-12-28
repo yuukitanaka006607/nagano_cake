fc0d103bb53ba38da6e4ae8d92b4ace2606beef3
YuukiTanaka <145189789+yuukitanaka006607@users.noreply.github.com>
2023-12-28 15:17:00 +0000

    [cart_item]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,7 +6,8 @@ class Public::CartItemsController < ApplicationController
     @cart_item.customer_id = current_customer.id
     if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
        cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-       #cart_item.amount += params[:cart_item][:amount].to_i cart_item.save
+       cart_item.amount += params[:cart_item][:amount].to_i
+       cart_item.save
        redirect_to cart_items_path
 
     else @cart_item.save
